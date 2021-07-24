45e2a4801931254d77cccc2b61dbcdcc48ed6c21
tella10 <77850544+tella10@users.noreply.github.com>
2021-07-24 12:05:20 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using email_app.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace email_app.Controllers
+{
+    public class HomeController : Controller
+    {
+		public ActionResult email(EmailData model)
+		{
+			ViewBag.Message = "Email page";
+
+			return View("email", model);
+		}
+
+		[HttpPost]
+		public string SendEmail(EmailData info)
+        {
+			SmtpClient client = new SmtpClient(info.MailServer);
+			client.Credentials = new NetworkCredential(info.MailUsername, info.MailPassword);
+			client.Port = info.MailPort;
+			client.EnableSsl = info.MailSSL;
+
+			MailMessage mail = new MailMessage();
+			mail.From = new MailAddress(info.MailFrom, "Email");
+			mail.Subject = "Medismarts Email from Server";
+			mail.Body = "Email Testing from the Server";
+
+			string[] receivers = info.Recipients;
+
+			try
+			{
+				for (int i = 0; i < receivers.Length; i++)
+				{
+					mail.To.Add(receivers[i]);
+					//mail.Bcc.Add("maskitq@yahoo.com");
+					//mail.CC.Add("uanusionwu@medismarts.com.ng");
+				}
+
+				client.Send(mail);
+				return "success";
+			}
+			catch (SmtpException ex)
+			{
+				string msg = ex.Message;
+				return msg;
+			}
+		}
+
+    }
+}
---
+++ Models/EmailData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace email_app.Models
+{
+    public class EmailData
+    {
+		//private string _MailServer = "smtp.gmail.com";
+		//private string _MailUsername = "Tellababatunde@gmail.com";
+		//private string _MailPassword = "veemncrlnbjezmur";
+		//private string _MailFrom = "Tellababatunde@gmail.com";   
+		
+		private string _MailServer;
+		private string _MailUsername;
+		private string _MailPassword;
+		private string _MailFrom;
+		private string _MailSubject = "";
+		private string[] _Recipients;
+		private string _Message;
+		private bool _IsHTML;
+		private string _DisplayName;
+
+
+
+		public string MailServer
+		{
+			get { return _MailServer; }
+			set { _MailServer = value; }
+		}
+
+		public string MailUsername
+		{
+			get { return _MailUsername; }
+			set { _MailUsername = value; }
+		}
+
+		public string MailPassword
+		{
+			get { return _MailPassword; }
+			set { _MailPassword = value; }
+		}
+
+		public string MailFrom
+		{
+			get { return _MailFrom; }
+			set { _MailFrom = value; }
+		}
+
+		public string MailSubject
+		{
+			get { return _MailSubject; }
+			set { _MailSubject = value; }
+		}
+
+		public string[] Recipients
+		{
+			get { return _Recipients; }
+			set { _Recipients = value; }
+		}
+
+		public string Message
+		{
+			get { return _Message; }
+			set { _Message = value; }
+		}
+
+        public string DisplayName
+        {
+            get { return _DisplayName; }
+            set { _DisplayName = value; }
+        }
+
+        public bool IsHTML
+		{
+			get { return _IsHTML; }
+			set { _IsHTML = value; }
+		}
+
+		public int MailPort { get; set; }
+
+		public bool MailSSL { get; set; }
+	}
+}
